36660624611972b270a44210861a3e7fda1e1f29
Robert <basehintz@gmail.com>
2020-11-20 13:42:25 -0700

    fleshed out Context.jsx for tweet page

--- sandboxclient/src/Context.jsx
+++ sandboxclient/src/Context.jsx
@@ -8,17 +8,17 @@ const Context = React.createContext();
 
 function ContextProvider({ children }) {
 
+    ////////////////hooks
     const [allTweets, setAllTweets] = useState([])
     const [allUsers, setAllUsers] = useState([])
+    const [tweetToReply, setTweetToReply] = useState({})
+    const [tweetToEdit, setTweetToEdit] = useState({})
+    const [userObject, setUserObject] = useState({});
+    const [viewUserId, setViewUserId] = useState("")
+    const [editTweet, setEditTweet] = useState(false)
 
-    //queries tweets database 
-    function queryTweets() {
-    fetch("http://localhost:3003/tweets")
-        .then((response) => response.json())
-        .then((json) => 
-        {setAllTweets(json)});
-}
-
+//////////////////////////////////////////
+/////////////////handle routes for tweets
     //queries user database
     function queryUsers() {
         fetch("http://localhost:3003/users")
@@ -26,6 +26,59 @@ function ContextProvider({ children }) {
         .then((json) => setAllUsers(json))
     }
 
+        //handles edit
+        function handleEdit(tweet) {
+            setEditTweet(true);
+            setTweetToEdit(tweet);
+            setTweetToReply({});
+        }
+
+        //queries tweets database 
+        function queryTweets() {
+        fetch("http://localhost:3003/tweets")
+            .then((response) => response.json())
+            .then((json) => 
+            {setAllTweets(json)});
+    }
+
+        //handles delete
+        function handleDelete(id) {
+            console.log("beginning delete", id);
+            fetch(`http://localhost:3003/tweets/${id}`, {
+                method: "DELETE",
+                headers: {
+                    'Content-Type': "application/json",
+                },
+                mode: "cors"
+            })
+            .then((response) => response.json())
+            .then((json) => {
+                //reloads page
+                queryTweets();
+                console.log(json)
+            })
+        }
+// ^ will take an id. The reply will be posted onto the tweet matching this id.
+
+
+///////////hits routes inside a subcategory of Tweets controllers/tweets 127
+function handleLike(id, bool) {
+    fetch(`http://localhost:3003/tweets/${id}/like`, {
+        method: "PATCH",
+        headers: {
+            "Content-type": "application/json",
+        },
+        body: JSON.stringify({ uid: userObject._id.toString(), bool: bool}),
+    })
+    .then((response) => response.json())
+    .then((json) => console.log("user likes updated", json, bool))
+}
+
+///////////////////////////////
+
+
+
+    //is used to call queryTweets and queryUsers on page read
     useEffect(() => {
         queryTweets();
         queryUsers();
@@ -34,10 +87,22 @@ function ContextProvider({ children }) {
     return(
     <Context.Provider 
         value={{
+            handleDelete,
             allTweets,
             queryTweets,
             allUsers,
             queryUsers,
+            handleEdit,
+            tweetToReply,
+            tweetToEdit,
+            handleLike,
+            setUserObject,
+            tweetToReply,
+            setTweetToReply,
+            viewUserId,
+            setViewUserId,
+            setEditTweet,
+            editTweet,
         }}>
             {children}
         </Context.Provider>
