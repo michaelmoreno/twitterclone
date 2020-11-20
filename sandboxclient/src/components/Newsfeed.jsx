18699216fe49a8078e5ae2f5dd7b2f7077bf49db
Robert <basehintz@gmail.com>
2020-11-19 22:00:07 -0700

    able to render things from the db to the tweets page

--- sandboxclient/src/components/Newsfeed.jsx
+++ sandboxclient/src/components/Newsfeed.jsx
@@ -2,6 +2,16 @@ import React, { Component, useContext } from 'react'
 import { Context } from '../Context.jsx'
 import Tweet from './Tweet.jsx';
 
+//============================================\\
+//===============Newsfeed Page================\\
+//Newsfeed is a page that for right now is    \\
+//used to display all of the tweets in the db \\
+//it should be a simple page that maps all of \\
+//the tweets. Not much should be need because \\
+//all of the magic should be happening in it's\\
+//child component <Tweet>                     \\
+//============================================\\
+
 function Newsfeed() {
 const {
     allTweets
--- sandboxclient/src/components/Tweet.jsx
+++ sandboxclient/src/components/Tweet.jsx
@@ -2,23 +2,21 @@ import React, { useState, useContext, useEffect } from "react";
 import { Link } from "react-router-dom";
 import { Context } from "../Context.jsx";
 
-function Tweet() {
+function Tweet(props) {
 
 
-    // let tweet = props.tweet;
+    let tweet = props.tweet;
 
-    // const {
-    //     allTweets,
-    // } = useContext(Context);
+    const {
+        allTweets,
+    } = useContext(Context);
 
-    // useEffect(() => {
-    //     let newArr = []
-    //     allTweets.forEach((i) => {
-    //         newArr.push(i)
-    //     })
-    // })
     return(
-        <div>Tweet page</div>
+        <div>
+            Tweet page
+        <div> {tweet.authorName} </div>
+        </div>
+    
     )
 }
 
